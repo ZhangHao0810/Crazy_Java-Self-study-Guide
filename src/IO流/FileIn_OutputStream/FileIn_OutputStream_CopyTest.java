851c544eab5fdeb95f77cfc56c55fffadc47c1dd
ZhangHao0810 <874993952@qq.com>
2019-10-05 23:34:57 +0800

    Io结尾

--- src/IO流/FileIn_OutputStream/FileIn_OutputStream_CopyTest.java
+++ src/IO流/FileIn_OutputStream/FileIn_OutputStream_CopyTest.java
@@ -10,14 +10,14 @@ import java.io.IOException;
  * @Description： IO的基类 用于输入输出字节流。
  * 输出流会自动创建新的文件。
  */
-public class FileIn_OutputStreamTest {
+public class FileIn_OutputStream_CopyTest {
     public static void main(String[] args) throws IOException {
-        FileInputStream fis=new FileInputStream("IO/File.txt");
+        FileInputStream fis=new FileInputStream("IO/File.txt"); //这里直接用字符串是因为字节流的构造应该输入的是File对象，但是输入字符串他可以自动new出来这个File对象。
         FileOutputStream fos=new FileOutputStream("IO/newFile.txt");
 
         byte[] buty=new byte[1024];
         int hasRead;
-        while ((hasRead=fis.read(buty))>0){
+        while ((hasRead=fis.read(buty))!=-1){ //这里的判断依据可以是！=-1 也可以是>0
             fos.write(buty,0,hasRead);
         }
     }
---
+++ src/IO流/System/SystemInOutStream.java
@@ -0,0 +1,12 @@
+package IO流.System;
+
+/**
+ * @Auther： ZhangHao
+ * @Date： 2019/10/5 22:19
+ * @Description： 标准输入输出流：
+ *      public static final InputStream in； 字节输入流。
+ *          public static final int x；
+ *          InputStream is=System.in;
+ */
+public class SystemInOutStream {
+}
